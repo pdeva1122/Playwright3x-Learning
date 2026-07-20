ad98f265e3e5566288c7a076a14dbb7f37adb2dc
Prashanth Devadiga Personal Laptop <pdeva1122@gmail.com>
2026-07-20 09:24:22 +0530

    Practice July 20

---
+++ 01_JavaScript_Basics/DataType.js
@@ -0,0 +1,35 @@
+// Data Type in the JS
+
+// string , Number , Bolean, nu;ll, undefined, object, symbol
+// array, NaN
+
+//Assignment Operator
+
+let a= 10;
+let b=3;
+
+//Arithmetic Operator
+
+let sum= a+b;
+let sub= a-b;
+let mul= a*b;
+let div= a/b;
+
+console.log("Sum: "+sum);
+console.log("Sub: "+sub);
+console.log("Mul: "+mul);
+console.log("Div: "+div);
+//Modulus 
+let mod= a%b;
+console.log("Modulus: "+mod);
+
+//Exponential **, power
+console.log("Exponential: "+(a**b));
+
+//
+let x1=10;
+x1+=5; // x1= x1+5
+console.log("x1: "+x1);
+x1-=5; // x1= x1-5
+console.log("x1: "+x1);
+
